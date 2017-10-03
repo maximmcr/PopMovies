e89bd8d9b29b325b112ccb7fc7d9262f2a7d777f
maximmcr <maximmcr@gmail.com>
2017-10-03 20:46:23 +0300

    Rename package name Fix ToolBar in SettingsActivity

---
+++ app/src/main/java/com/maximmcr/android/popmovies/FABScrolling.java
@@ -0,0 +1,39 @@
+package com.maximmcr.android.popmovies;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Frei on 28.07.2017.
+ */
+
+public class FABScrolling extends FloatingActionButton.Behavior {
+
+    public FABScrolling(Context context, AttributeSet attrs) {
+        super();
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child,
+                               View target, int dxConsumed, int dyConsumed, int dxUnconsumed,
+                               int dyUnconsumed) {
+
+        super.onNestedScroll(coordinatorLayout, child, target,
+                dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+
+        if (child.getVisibility() == View.VISIBLE && dyConsumed > 0) {
+            child.hide();
+        } else if (child.getVisibility() == View.GONE && dyConsumed < 0) {
+            child.show();
+        }
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+}
---
+++ app/src/main/java/com/maximmcr/android/popmovies/VideoAdapter.java
@@ -0,0 +1,81 @@
+package com.maximmcr.android.popmovies;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.maximmcr.android.popmovies.model.VideoModel;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Frei on 10.07.2017.
+ */
+
+public class VideoAdapter extends ArrayAdapter<VideoModel> {
+
+    private static final String YOUTUBE_REQUEST_BASE = "https://www.youtube.com/watch";
+
+    private Context mContext;
+
+    private static class ViewHolder {
+        TextView type;
+        TextView name;
+    }
+
+    public VideoAdapter(Context context, ArrayList<VideoModel> videoModels) {
+        super(context, R.layout.list_item_video, videoModels);
+        mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        final VideoModel videoModel = getItem(position);
+
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            convertView = inflater.inflate(R.layout.list_item_video, parent, false);
+            viewHolder.name = (TextView) convertView.findViewById(R.id.video_name);
+            viewHolder.type = (TextView) convertView.findViewById(R.id.video_type);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.type.setText(videoModel.getType());
+        viewHolder.name.setText(videoModel.getName());
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Utility.isOnline(mContext)) {
+                    Uri uri = Uri.parse(YOUTUBE_REQUEST_BASE)
+                            .buildUpon()
+                            .appendQueryParameter("v", videoModel.getPath())
+                            .build();
+                    Intent playVideo = new Intent(Intent.ACTION_VIEW, uri);
+                    if (playVideo.resolveActivity(mContext.getPackageManager()) != null) {
+                        Intent chooser = Intent.createChooser(playVideo, "Play via");
+                        chooser.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        mContext.startActivity(chooser);
+                    }
+                } else {
+                    Snackbar.make(v, "There is no internet connection!", Snackbar.LENGTH_LONG)
+                            .show();
+                }
+            }
+        });
+
+        return convertView;
+    }
+}
